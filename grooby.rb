0f7b13006159a50b6624cac0af69c7f235e4e3f5
Crispin Müller <crispin.mueller@9elements.com>
2018-05-19 20:34:51 +0200

    get songs through uid (folder name)

--- grooby.rb
+++ grooby.rb
@@ -2,10 +2,20 @@ require 'mfrc522'
 require 'ruby-mpd'
 
 $r = MFRC522.new
-$mpd = MPD.new
-$mpd.connect
-$mpd.stop
-$mpd.clear
+
+def init_mpd
+  $mpd = MPD.new
+  $mpd.connect
+  $mpd.stop
+  $mpd.clear
+end
+
+def start_playlist(card_uid)
+  $mpd.stop
+  $mpd.clear
+  $mpd.where({file: card_uid}, {add: true})
+  $mpd.play
+end
 
 def card_control
   while(true)
@@ -14,20 +24,7 @@ def card_control
       if $r.picc_request(MFRC522::PICC_REQA)
         uid, sak = $r.picc_select
         uid = uid.pack('C*').unpack('H*')[0]
-        case uid
-        when "195ba9c5"
-          $mpd.stop
-          $mpd.clear
-          $mpd.where({ album: "Phantasma"}, {add:true})
-          $mpd.play
-        when "f4771be3"
-          $mpd.stop
-          $mpd.clear
-          $mpd.where({ album: "Dilation"}, {add:true})
-          $mpd.play
-        else
-          puts "unbekannte Karte"
-        end
+        start_playlist(uid)
         $r.picc_halt
       end
 
@@ -40,6 +37,8 @@ def card_control
     sleep(0.2)
   end
 end
+
+init_mpd()
 
 card_thread = Thread.new{card_control()}
 card_thread.join
